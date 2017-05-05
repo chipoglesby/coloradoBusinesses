e0039387938b851a5c088ad992b356d7f2cde392
Chip Oglesby <chip@creativesparkcolumbia.com>
2017-05-05 11:05:47 -0600

    Adding some more cleaning

--- code/cleaningData.R
+++ code/cleaningData.R
@@ -4,6 +4,7 @@ rm(entities)
 
 entitiesCleaned$entityformdate <- 
   as.Date(entitiesCleaned$entityformdate, '%m/%d/%Y')
+
 entitiesCleaned[sapply(entitiesCleaned, is.character)] <- 
   lapply(entitiesCleaned[sapply(entitiesCleaned, is.character)], tolower)
 
@@ -12,3 +13,8 @@ write_csv(entitiesCleaned, "data/entitiesCleaned.csv")
 entities <- entitiesCleaned
 
 rm(entitiesCleaned)
+
+
+entitiesCleaned$latitude <- str_extract(entitiesCleaned$location, "\\d{2}\\.\\d{1,}")
+entitiesCleaned$longitude <- str_extract(entitiesCleaned$location, "\\-\\d{2}\\.\\d{1,}")
+entitiesCleaned$standing <- str_extract(entitiesCleaned$entityname, '\\,\\s.*')
